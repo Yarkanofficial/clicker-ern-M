3b1d15bb668e3f5a6dcb9f1a017fe896354cc491
Yarkanofficial <Yarkanofficial@gmail.com>
2026-07-27 13:14:26 +0300

    Rename 'timeleft' to 'timerEnd' in user document

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ if (registerBtn) {
         email: email,
         clicks: 0,
         coins: 0,
-        timeleft: Date.now() + (24 * 60 * 60 * 1000),
+        timerEnd: Date.now() + (24 * 60 * 60 * 1000),
         createdAt: serverTimestamp()
       });
 
